a954f71abc66ed13d8d93870d8762c908b5bd64b
hwm <hwm@mm.local>
2016-03-15 16:38:31 +0800

    自定义tabBar,增加分类,增加中间按钮

---
+++ baisibudejie/baisibudejie/UIView+WMExtension.h
@@ -0,0 +1,23 @@
+//
+//  UIView+WMExtension.h
+//  baisibudejie
+//
+//  Created by hwm on 16/3/15.
+//  Copyright © 2016年 hwm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (WMExtension)
+
+@property (nonatomic, assign) CGFloat wm_width;
+@property (nonatomic, assign) CGFloat wm_height;
+@property (nonatomic, assign) CGFloat wm_x;
+@property (nonatomic, assign) CGFloat wm_y;
+@property (nonatomic, assign) CGFloat wm_centerX;
+@property (nonatomic, assign) CGFloat wm_centerY;
+
+@property (nonatomic, assign) CGFloat wm_right;
+@property (nonatomic, assign) CGFloat wm_bottom;
+
+@end
--- baisibudejie/baisibudejie/WMTabBarController.m
+++ baisibudejie/baisibudejie/WMTabBarController.m
@@ -7,45 +7,17 @@
 //
 
 #import "WMTabBarController.h"
+#import "WMTabBar.h"
 
 @interface WMTabBarController ()
 
-@property (nonatomic, strong) UIButton *publishButton;
+
 
 @end
 
 @implementation WMTabBarController
 
-- (UIButton *)publishButton {
-    
-    if (!_publishButton) {
-        _publishButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_publishButton setImage:[UIImage imageNamed:@"tabBar_publish_icon"] forState:UIControlStateNormal];
-        [_publishButton setImage:[UIImage imageNamed:@"tabBar_publish_click_icon"] forState:UIControlStateSelected];
-        _publishButton.frame = CGRectMake(0, 0, self.tabBar.frame.size.width / 5, self.tabBar.frame.size.height);
-        _publishButton.center = CGPointMake(self.tabBar.frame.size.width / 2, self.tabBar.frame.size.height / 2);
-        [_publishButton addTarget:self action:@selector(publishClick) forControlEvents:UIControlEventTouchUpInside];
-        
-    }
-    
-    return _publishButton;
-}
 
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    
-    [self.tabBar addSubview:self.publishButton];
-}
-
-#pragma mark -- 点击更多
-- (void)publishClick {
-    
-    UIViewController *contro = [[UIViewController alloc] init];
-    contro.view.backgroundColor = [UIColor yellowColor];
-    
-    [self presentViewController:contro animated:YES completion:nil];
-    
-}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -67,6 +39,8 @@
     // 添加子控制器
     [self setUpChildController];
     
+    [self setValue:[[WMTabBar alloc] init] forKey:@"tabBar"];
+    
 }
 
 - (void)setUpChildController {
@@ -74,8 +48,6 @@
     [self setUpOneChildController:[[UINavigationController alloc] initWithRootViewController:[[UIViewController alloc] init]] image:[UIImage imageNamed:@"tabBar_essence_icon"]  selectImage:[UIImage imageNamed:@"tabBar_essence_click_icon"] title:@"精华"];
     
     [self setUpOneChildController:[[UINavigationController alloc] initWithRootViewController:[[UIViewController alloc] init]] image:[UIImage imageNamed:@"tabBar_essence_icon"]  selectImage:[UIImage imageNamed:@"tabBar_essence_click_icon"] title:@"精华"];
-    
-    [self setUpOneChildController:[[UINavigationController alloc] initWithRootViewController:[[UIViewController alloc] init]] image:nil  selectImage:nil title:nil];
     
     [self setUpOneChildController:[[UINavigationController alloc] initWithRootViewController:[[UIViewController alloc] init]] image:[UIImage imageNamed:@"tabBar_essence_icon"]  selectImage:[UIImage imageNamed:@"tabBar_essence_click_icon"] title:@"精华"];
     
@@ -85,8 +57,11 @@
 
 - (void)setUpOneChildController:(UIViewController *)vc image:(UIImage *)image selectImage:(UIImage *)selectImage title:(NSString *)title {
     
+    
+    vc.view.backgroundColor = WMRandomColor;
     vc.tabBarItem.title = title;
     vc.tabBarItem.image = image;
+    
     vc.tabBarItem.selectedImage = selectImage;
     [self addChildViewController:vc];
     
