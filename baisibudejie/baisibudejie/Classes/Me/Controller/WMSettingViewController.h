653f9ef0c074bfb86c4790d8054336f2383cacd7
hwm <hwm@mm.local>
2016-03-17 19:54:46 +0800

    计算缓存

--- baisibudejie/baisibudejie/Classes/Me/Controller/WMSettingViewController.h
+++ baisibudejie/baisibudejie/Classes/Me/Controller/WMSettingViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface WMSettingViewController : UIViewController
+@interface WMSettingViewController : UITableViewController
 
 @end
--- baisibudejie/baisibudejie/Classes/Me/Controller/WMSettingViewController.m
+++ baisibudejie/baisibudejie/Classes/Me/Controller/WMSettingViewController.m
@@ -10,9 +10,15 @@
 
 @interface WMSettingViewController ()
 
+@property (nonatomic, assign) unsigned long long size;
+
 @end
 
 @implementation WMSettingViewController
+
+- (instancetype)init {
+    return [self initWithStyle:UITableViewStyleGrouped];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -20,9 +26,64 @@
     
     self.navigationItem.title = @"我的设置";
     
-    self.view.backgroundColor = WMRandomColor;
+    self.tableView.backgroundColor = WMCommonBgColor;
+    
+    // 计算文件总大小
+    [self getCacheSize];
+    
+}
+
+- (void)getCacheSize {
+    
+    // 总大小
+//    unsigned long long size = 0;
+    
+    // 沙盒文件夹路径
+    NSString *cachesPath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).lastObject;
+    
+    NSString *dirPath = [cachesPath stringByAppendingPathComponent:@"default"];
+    
+    // 文件管理者
+    NSFileManager *manage = [NSFileManager defaultManager];
+    
+    // 文件夹中的所有文件
+    NSArray *subPaths = [manage subpathsAtPath:dirPath];
+    
+    for (NSString *subPath in subPaths) {
+        
+        // 所有文件的路径
+        NSString *fullSubPath = [dirPath stringByAppendingPathComponent:subPath];
+        
+        // 所有文件的总大小
+        self.size += [manage attributesOfItemAtPath:fullSubPath error:nil].fileSize;
+        
+    }
+    
+    WMLog(@"%zd", self.size);
     
     
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *settingID = @"setting";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:settingID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:settingID];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"清除缓存%zd",self.size];
+    
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
